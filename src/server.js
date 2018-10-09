384593befcde2d28ae38e9c24759112d273b84d8
Vladislav Plemyannik <razz2zz@outlook.com>
2018-10-09 22:27:50 +0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(5000, () => {
+    console.log("Server started on 5000 port")
+});
